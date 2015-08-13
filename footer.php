1b5b6c9f01afdf89506bc39d761af321d11c3342
nicole warnemuende <nmuende@gmail.com>
2015-08-12 18:30:08 -0700

    Updates widths and styles

--- footer.php
+++ footer.php
@@ -1,42 +1,48 @@
 <!--begin footer-->
-	<footer>
-		<div id="footer-left" class="footer-div">
+	</div><!--end outside-->
 
-				<p>The Black Heritage Society is a<br> 501(c)(3) non-profit organization</p>
-				<p><strong><?php print ("&copy; " . date ('Y') . " "); ?> The Black Heritage Society of Washington State</strong></p>
+	<div id="low-bar">
+	</div><!--end low-bar-->
+	
+	<div id="outer-footer">
+		<footer>
+			<div id="footer-left" class="footer-div">
 
-		</div><!--end footer-left-->
+					<p>The Black Heritage Society is a<br> 501(c)(3) non-profit organization</p>
+					<p><strong><?php print ("&copy; " . date ('Y') . " "); ?> The Black Heritage Society of Washington State</strong></p>
 
-		<div id="footer-middle-left" class="footer-div">
-			<?php wp_nav_menu( array(
-				'theme_location' => 'footer-menu-1' ,
-				'menu' => 'Footer Menu 1' ,
-				'container'  => 'ul',
-				'depth' => 0,
-				'link_before' => '<span>',
-				'link_after' => '</span>',
-			)); ?>
-		</div><!--end footer-middle-left-->
+			</div><!--end footer-left-->
 
-		<div id="footer-middle-right" class="footer-div">
-			<?php wp_nav_menu( array(
-				'theme_location' => 'footer-menu-2' ,
-				'menu' => 'Footer Menu 2' ,
-				'container'  => 'ul',
-				'depth' => 0,
-				'link_before' => '<span>',
-				'link_after' => '</span>',
-			)); ?>
-		</div><!--end footer-middle-right-->
+			<div id="footer-middle-left" class="footer-div">
+				<?php wp_nav_menu( array(
+					'theme_location' => 'footer-menu-1' ,
+					'menu' => 'Footer Menu 1' ,
+					'container'  => 'ul',
+					'depth' => 0,
+					'link_before' => '<span>',
+					'link_after' => '</span>',
+				)); ?>
+			</div><!--end footer-middle-left-->
 
-		<div id="footer-right" class="footer-div"	>
-			<a href="http://www.mohai.org/"><img src="<?php bloginfo('template_url');?>/images/Mohai_Logo.png" alt="Mohai Logo" ?></a>
-		</div><!--end footer-right-->
+			<div id="footer-middle-right" class="footer-div">
+				<?php wp_nav_menu( array(
+					'theme_location' => 'footer-menu-2' ,
+					'menu' => 'Footer Menu 2' ,
+					'container'  => 'ul',
+					'depth' => 0,
+					'link_before' => '<span>',
+					'link_after' => '</span>',
+				)); ?>
+			</div><!--end footer-middle-right-->
 
-	</footer>
+			<div id="footer-right" class="footer-div"	>
+				<a href="http://www.mohai.org/"><img src="<?php bloginfo('template_url');?>/images/Mohai_Logo.png" alt="Mohai Logo" ?></a>
+			</div><!--end footer-right-->
+
+		</footer>
+	</div><!--outer footer-->
 
 	<?php wp_footer(); ?>
 
-</div><!--end wrapper-->
 </body>
 </html><!--end footer-->
