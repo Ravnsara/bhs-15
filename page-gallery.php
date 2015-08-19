ef0b14625f3255f156bd7a46ecbb2ce46531f015
nicole warnemuende <nmuende@gmail.com>
2015-08-19 15:45:18 -0700

    Updates gallery, attachment page and styles

--- page-gallery.php
+++ page-gallery.php
@@ -5,27 +5,27 @@
 ?>
 <?php get_header(); ?>
 <!--begin content-->
-			<div id="attachment-content" class="attachment">
+			<div id="gallery-content" class="attachment">
 				<ul class="attachment-list">
 					<?php
 
-						$args = array(
-							'post_type' => 'attachment',
-							'posts_per_page' => -1,
-							'post_status' => null,
-							'post_mime_type' => 'image',
-							'post_parent' => $post->ID
-						);
+					$args = array(
+						'post_type' => 'attachment',
+						'posts_per_page' => -1,
+						'post_status' => null,
+						'post_mime_type' => 'image',
+						'post_parent' => $post->ID
+					);
 
-						$images = get_posts( $args );
-						
-						if ( $images ) {	
-							foreach ( $images as $image ) {    
-								echo '<li class="attachment-image">';
-								echo wp_get_attachment_image( $image->ID, 'gallery' );
-								echo '</li>'; 
-							}
-						} ?>
+					$images = get_posts( $args );
+					
+					if ( $images ) {	
+						foreach ( $images as $image ) : ?>
+							<li class="attachment-image" onclick="location.href='<?php echo $link; ?>'">
+							<?php echo the_attachment_link( $image->ID, 'gallery' ); ?>
+							</li> 
+						<?php endforeach;
+					} ?>
 				</ul>
 			</div><!--end attachment-content-->
 		</div><!--end main-inside-->	
